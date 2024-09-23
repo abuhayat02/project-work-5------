05ddc12e322e186bc8fff7ba4b4244c2d9e79177
Module 3 <abuhayat5mmm@gmail.com>
2024-09-24 02:36:05 +0600

    color combination

--- js/logic.js
+++ js/logic.js
@@ -4,17 +4,17 @@ let transcriptionHistory = getId('history-transection');
 let dontContent = getId("donet-contant")
 
 donet.addEventListener('click', function(event){
-    classAdd(donet , 'bg-lime-300')
+    classAdd(donet , 'bg-blue-300')
     classAdd(transcriptionHistory,'hidden')
-    classRemove(history , 'bg-lime-300')
+    classRemove(history , 'bg-blue-300')
     classRemove(dontContent , 'hidden')
 
     
 })
 
 history.addEventListener('click' , function(){
-    classRemove(donet , 'bg-lime-300')
-    classAdd(history , 'bg-lime-300');
+    classRemove(donet , 'bg-blue-300')
+    classAdd(history , 'bg-blue-300');
     classRemove(transcriptionHistory , 'hidden')
     classAdd(dontContent,'hidden')
 
