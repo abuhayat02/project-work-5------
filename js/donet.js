1f4c42a7bebdecc518b5e759b27eea91820e16df
Module 3 <abuhayat5mmm@gmail.com>
2024-09-24 01:32:23 +0600

    wow ! it's amazing project with javascript !

---
+++ js/donet.js
@@ -0,0 +1,5 @@
+// function donetCalculation(btnId , inputBalanceId , oldBalanceId , balance ,loction) {
+
+donetCalculation('btn-donet-noakhali','donet-noakhali','balance-noakhali','main-balance','location-1');
+donetCalculation('btn-donet-feni','donet-feni','balance-feni','main-balance','location-2');
+donetCalculation('btn-donet-quota','donet-quota','balance-quota','main-balance','location-3');
--- js/utilites.js
+++ js/utilites.js
@@ -3,6 +3,21 @@ function getId(id){
     return docsId;
 }
 
+
+function convartIntWithInput(element){
+    let valueFormId =parseFloat(element.value);
+    return valueFormId ;
+
+
+}
+
+
+function convartIntWithTag(element){
+    let valueFormId = parseFloat(element.innerText);
+    return valueFormId ;
+
+}
+
 function classAdd(element , className){
 
     element.classList.add(className);
@@ -11,3 +26,52 @@ function classAdd(element , className){
 function classRemove(element,className){
     element.classList.remove(className);
 }
+function showModal(){
+    let alartBox = getId('alart-box');
+
+    classRemove(alartBox , 'hidden')
+    classAdd(alartBox , 'flex');
+}
+function closeModal(){
+    let alartBox = getId('alart-box');
+    classRemove(alartBox , 'flex')
+    classAdd(alartBox , 'hidden');
+}
+
+function donetCalculation(btnId , inputBalanceId , oldBalanceId , balance ,loction) {
+    // function donetCalculation() {
+        let munth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        let location = getId(loction)
+        let donetButton = getId(btnId);
+        let getDonet = getId(inputBalanceId);
+        let balanceBox = getId(oldBalanceId);
+        let mainBalance = getId(balance);
+    
+        donetButton.addEventListener('click', function () {
+            let getDonetBalance = convartIntWithInput(getDonet);
+            let oldbalance = convartIntWithTag(balanceBox);
+            if (getDonetBalance > 0 && convartIntWithTag(mainBalance) >= getDonetBalance) {
+                let myDate = new Date();
+                let div = document.createElement('div')
+                let detalse = getId("history-transection");
+                div.innerHTML = ` 
+                <div class="border border-gray-300  rounded-md p-4 my-3 space-y-3">
+                    <h1 class="text-xl font-bold">${getDonetBalance} Taka is Donated for famine- ${myDate.getFullYear()}  at ${location.innerText} Bangladesh</h1>
+                    <p class="font-extralight text-sm">Date :${week[myDate.getDay()]} ${munth[myDate.getMonth() + 1]} ${myDate.getDate()} ${myDate.getFullYear()} and time ${myDate.getHours()}:${myDate.getMinutes()} <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX"></iframe></noscript> GMT +0600 (Bangladesh Standard Time)</p>
+                </div>`;
+                detalse.appendChild(div)
+                balanceBox.innerText = getDonetBalance + oldbalance;
+                mainBalance.innerText = convartIntWithTag(mainBalance) - (getDonetBalance + oldbalance)
+    
+    
+                showModal()
+    
+            } else {
+                alert('somting wrong');
+                return;
+            }
+    
+        })
+    
+    }
